1c09b711e63de23109e36a0487b838ac98120f94
Данила Глазунов <glazdan17@gmail.com>
2022-04-24 12:03:47 +0300

    git init

---
+++ MyStack_Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyStack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
